4317ce769383087e5da00e6acec237f4d987e2b4
JasonCodez <ryznfury@gmail.com>
2025-11-06 01:17:18 -0600

    Fix apple touch icon reference to SVG

--- app/layout.js
+++ app/layout.js
@@ -63,7 +63,7 @@ export default function RootLayout({ children }) {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Propstars" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.svg" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
